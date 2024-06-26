839d05cd69599b182704798b6f79dd57f4f196f5
Alex Diaz <alex.diaz@540deg.com>
2024-06-26 12:03:40 +0200

    [] - Separar test unitarios y test de integracion

--- laravel/Status/Tests/StatusControllerTest.php
+++ laravel/Status/Tests/StatusControllerTest.php
@@ -2,9 +2,11 @@
 
 namespace Laravel\Status\Tests;
 
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
+#[Group("Integration")]
 class StatusControllerTest extends TestCase
 {
     #[test]
